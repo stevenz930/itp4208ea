bd06fc077bfb08376fb2a2df4879e19f34240b6a
StevenZ <734274261@qq.com>
2025-04-07 05:44:06 +0800

    20250407
    -create home_course_recommand.html
    -create home_instructor_recommand.html
    -create course_detail.html
    -organize files with js, css in js.html & css.html

--- main/courses/urls.py
+++ main/courses/urls.py
@@ -1,9 +1,10 @@
 # courses/urls.py
 from django.urls import path
 from . import views
-from .views import course_list
+from .views import course_list, course_detail
 # from .views import courses
 
 urlpatterns = [
     path('course_list/', course_list, name='course_list'),
+    path('detail/<int:course_id>/', course_detail, name='course_detail'),
 ]
